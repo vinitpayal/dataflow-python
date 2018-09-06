159ef5906577885a396cb3a7cc2b9a7f225683ce
vinitpayal <vinitpayal@gmail.com>
2018-09-06 17:26:22 +0530

    word count with inmemory pcollection & conditional processing type based on passed argument

--- examples/word_count.py
+++ examples/word_count.py
@@ -7,10 +7,11 @@ from apache_beam.pipeline import SetupOptions
 import argparse
 import re
 
+DEFAULT_WRITE_FILE = '/tmp/test.txt'
 
 class WordExtractingDoFn(beam.DoFn):
-    def __init__(self):
-        super(WordExtractingDoFn, self).__init__()
+    # def __init__(self):
+    #     super(WordExtractingDoFn, self).__init__()
 
     def process(self, element):
         text_line = element.strip()
@@ -29,10 +30,17 @@ def run(argv=None):
                         help='Input file to process.')
     parser.add_argument('--output',
                         dest='output',
-                        required=True,
                         help='Output file to write results to.')
+    parser.add_argument('--execution_type',
+                        dest='execution_type',
+                        default='file-read',
+                        help='Type of execution for this script')
 
     known_args, pipeline_args = parser.parse_known_args(argv)
+
+    if known_args.execution_type == 'in-memory':
+        test_inmemory_word_count()
+        return
 
     pipeline_options = PipelineOptions(pipeline_args)
 
@@ -57,10 +65,44 @@ def run(argv=None):
               | 'count' >> beam.Map(count_ones)
               | 'format' >> beam.Map(format_result)
               )
-    counts | 'write' >> WriteToText(known_args.output)
+    counts | 'write' >> WriteToText(known_args.output if known_args.output else DEFAULT_WRITE_FILE)
+
+    result = p.run()
+    result.wait_until_finish()
+
+
+def test_inmemory_word_count(argv=None):
+    words_list = ["hey", "hi", "ok", "good", "hey", "nice", "ok", "yo"]
+    p = beam.Pipeline()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output',
+                        dest='output',
+                        help='Output file to write results to.')
+
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    def print_in_console(word_string_with_count):
+        print(word_string_with_count)
+        return word_string_with_count
+
+    words = (p
+             | 'create pcollection from inmemory' >> beam.Create(words_list)
+             | 'pair' >> beam.Map(lambda x: (x, 1))
+             | 'grouping' >> beam.GroupByKey()
+             | 'count' >> beam.Map(lambda word_count_map: (word_count_map[0], sum(word_count_map[1])))
+             | 'format' >> beam.Map(lambda word_result: ('%s: %d' % (word_result[0], word_result[1])))
+             )
+
+    # don't store updated pcollection just used for printing
+    words | 'console output' >> beam.Map(print_in_console)
+
+    words | 'write' >> WriteToText(known_args.output if known_args.output else DEFAULT_WRITE_FILE)
 
     result = p.run()
     result.wait_until_finish()
 
 
 run()
+
+
